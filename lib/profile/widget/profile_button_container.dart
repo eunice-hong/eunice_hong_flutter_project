ac6f3ccf9cf941430497aadf68db437b54f73d89
Eunjin Hong <76774659+eunice-hong@users.noreply.github.com>
2023-05-06 14:23:45 +0900

    refactor: add Missing a required trailing comma

--- lib/profile/widget/profile_button_container.dart
+++ lib/profile/widget/profile_button_container.dart
@@ -91,8 +91,10 @@ class ProfileButtonContainer extends StatelessWidget {
 
   String? encodeQueryParameters(Map<String, String> params) {
     return params.entries
-        .map((MapEntry<String, String> e) =>
-            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+        .map(
+          (MapEntry<String, String> e) =>
+              '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}',
+        )
         .join('&');
   }
 }
